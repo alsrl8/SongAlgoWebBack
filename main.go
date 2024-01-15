54014c943c0ae8376dc7e1801317acb6d16fa9c2
alsrl8 <alsrl8@naver.com>
2024-01-15 11:25:49 +0900

    Port 변경

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	router.GET("/chat", chat.RequestHandler)
 
-	err := router.Run("localhost:8080")
+	err := router.Run("localhost:80")
 	if err != nil {
 		fmt.Printf("Error running gin router: %s", err)
 		return
