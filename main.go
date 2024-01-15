e68bc11b3218eb1975305ea2aba1b3a1dc6d2063
smg <alsrl8@naver.com>
2024-01-16 06:10:26 +0900

    Certificate

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func main() {
 
 	router.GET("/chat", chat.RequestHandler)
 
-	err := router.Run("0.0.0.0:8080")
+	//err := router.Run("0.0.0.0:8080")
+
+	certFile := "/etc/letsencrypt/live/server.songmingi.com/fullchain.pem"
+	keyFile := "/etc/letsencrypt/live/server.songmingi.com/privkey.pem"
+	err := router.RunTLS("0.0.0.0:800", certFile, keyFile)
+
 	if err != nil {
 		fmt.Printf("Error running gin router: %s", err)
 		return
